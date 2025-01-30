e2629637ac0f0d103abb630230c0fa0ad9c792ea
Yemi Ajibola <yajibola23@gmail.com>
2025-01-30 12:26:00 -0600

    Created model class featuring SwiftData.

---
+++ ToDoSwiftUI/ToDoItem.swift
@@ -0,0 +1,24 @@
+//
+//  ToDoItem.swift
+//  ToDoSwiftUI
+//
+//  Created by Yemi Ajibola on 1/30/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class ToDoItem {
+    var title: String
+    var timestamp: Date
+    var isCompleted: Bool
+    var isCritical: Bool
+    
+    init(title: String, timestamp: Date = .now, isCompleted: Bool = false, isCritical: Bool = false) {
+        self.title = title
+        self.timestamp = timestamp
+        self.isCompleted = isCompleted
+        self.isCritical = isCritical
+    }
+}
