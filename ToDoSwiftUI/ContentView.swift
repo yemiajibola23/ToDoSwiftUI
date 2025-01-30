95d8069a960afe9c0bfd68e74a7f3a3a18598629
Yemi Ajibola <yajibola23@gmail.com>
2025-01-30 17:57:44 -0600

    Updated UI of content view and data is persisting through app launches.

--- ToDoSwiftUI/ContentView.swift
+++ ToDoSwiftUI/ContentView.swift
@@ -6,28 +6,64 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
     @State var showCreateView = false
+    @Query private var items: [ToDoItem]
     
     var body: some View {
         NavigationStack {
-            Text("Hello, world!")
-                .toolbar {
-                    ToolbarItem {
-                        Button {
-                            showCreateView.toggle()
-                        } label: {
-                            Label("Add Item", systemImage: "plus")
+            List {
+                ForEach(items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            if item.isCritical {
+                                Image(systemName: "exclamationmark.2")
+                                    .symbolVariant(.fill)
+                                    .foregroundStyle(Color.red)
+                                    .font(.largeTitle)
+                                    .bold()
+                                
+                            }
+                            Text(item.title)
+                                .font(.largeTitle)
+                                .bold()
+                            
+                            Text("\(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .shortened))")
+                                .font(.callout)
                         }
+                        
+                        Spacer()
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "checkmark")
+                                .symbolVariant(.circle.fill)
+                                .foregroundStyle(item.isCompleted ? Color.green : Color.gray)
+                                .font(.largeTitle)
+                        }
+                        .buttonStyle(.plain)
                     }
                 }
-                .sheet(isPresented: $showCreateView) {
-                    NavigationStack {
-                        CreateToDoView()
+            }
+            .navigationTitle("My Todos")
+            .toolbar {
+                ToolbarItem {
+                    Button {
+                        showCreateView.toggle()
+                    } label: {
+                        Label("Add Item", systemImage: "plus")
                     }
-                    .presentationDetents([.medium])
                 }
+            }
+            .sheet(isPresented: $showCreateView) {
+                NavigationStack {
+                    CreateToDoView()
+                }
+                .presentationDetents([.medium])
+            }
         }
         .padding()
     }
